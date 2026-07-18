5ad2a4502e4d5aa377389b978df285cfaa8d379a
Rohan Variankaval <29784601+rdfds@users.noreply.github.com>
2026-07-17 20:53:06 -0400

    test(causal-impact-model): cover FailingHGBPredictor

--- tests/test_causal_impact_model.py
+++ tests/test_causal_impact_model.py
@@ -29,3 +29,32 @@ class _TreeStub:
         self.nodes = nodes
 
 
+class _FailingHGBPredictor:
+    def __init__(self) -> None:
+        self.learning_rate = 0.1
+        self._baseline_prediction = 1.0
+        dtype = [
+            ("value", "<f8"),
+            ("count", "<u4"),
+            ("feature_idx", "<u4"),
+            ("num_threshold", "<f8"),
+            ("missing_go_to_left", "u1"),
+            ("left", "<u4"),
+            ("right", "<u4"),
+            ("gain", "<f8"),
+            ("depth", "<u4"),
+            ("is_leaf", "u1"),
+            ("bin_threshold", "u1"),
+            ("is_categorical", "u1"),
+            ("bitset_idx", "<u4"),
+        ]
+        nodes = np.zeros(3, dtype=dtype)
+        nodes[0] = (0.0, 10, 0, 0.5, 0, 1, 2, 0.0, 0, 0, 0, 0, 0)
+        nodes[1] = (2.0, 5, 0, 0.0, 0, 1, 1, 0.0, 1, 1, 0, 0, 0)
+        nodes[2] = (-1.0, 5, 0, 0.0, 0, 2, 2, 0.0, 1, 1, 0, 0, 0)
+        self._predictors = [[_TreeStub(nodes)]]
+
+    def predict(self, X):  # noqa: N803
+        raise ValueError("synthetic omp failure")
+
+
